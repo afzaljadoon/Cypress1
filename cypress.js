3b43f31d78607f5de4f6b7d89112148239c04a8c
Afzal Khan Jadoon <116053189+afzaljadoon@users.noreply.github.com>
2024-01-19 19:19:07 +0500

    this test case is to verify that user enter valid data

--- cypress.js
+++ cypress.js
@@ -5,3 +5,12 @@ it('should have necessary UI elements for adding a book', () => {
     cy.get('#submit-button').should('exist');
   });
   
+  it('should add a new book successfully', () => {
+    cy.visit('/add-book');
+    cy.get('#title-input').type('Sample Book');
+    cy.get('#author-input').type('John Doe');
+    cy.get('#submit-button').click();
+    cy.url().should('include', '/books');
+    cy.contains('Sample Book by John Doe').should('exist');
+  });
+  
